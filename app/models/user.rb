2a0ba385bbadec28d033d015191a677770572a2e
enricostano <ocirne.onats@gmail.com>
2012-11-18 18:33:50 +0100

    add validate presence of password_confirmation in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
-  validates_presence_of :name
+  validates_presence_of :name, :password_confirmation
 end
