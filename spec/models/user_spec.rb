961ca0fde748b0119ad5e950e91a803912ea8c50
enricostano <ocirne.onats@gmail.com>
2012-11-18 18:30:28 +0100

    add spec User is invalid without a name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,7 @@ describe User do
   it "is invalid without an email" do
     FactoryGirl.build(:user, email: nil).should_not be_valid
   end
+  it "is invalid without a name" do
+    FactoryGirl.build(:user, name: nil).should_not be_valid
+  end
 end
