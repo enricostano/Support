013ec0ad3ba3a5b3722f00c142ab900c2a63e81c
enricostano <ocirne.onats@gmail.com>
2012-11-18 21:51:19 +0100

    add feature spec failing without name for user sign up

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -13,4 +13,17 @@ feature "user sign up" do
 
     expect(page).to have_text("Welcome Rebel Rebel!")
   end
+  context "sign up with invalid data" do
+    scenario "doesn't fill name" do
+      visit '/users/sign_up'
+    
+      fill_in "Email", with: "rebel@rebel.reb"
+      fill_in "user_password", with: "SoSecret"
+      fill_in "user_password_confirmation", with: "SoSecret"
+      click_button "Sign up"
+
+      expect(page).to have_text("Name can't be blank")
+    end
+  end
+
 end
