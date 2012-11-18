1ec0c86ac7623e888bacbd0648dc4be04c48bba4
enricostano <ocirne.onats@gmail.com>
2012-11-18 18:29:43 +0100

    add spec User is invalid without an email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,7 @@ describe User do
   it "has a valid factory" do
     FactoryGirl.create(:user).should be_valid
   end
+  it "is invalid without an email" do
+    FactoryGirl.build(:user, email: nil).should_not be_valid
+  end
 end
